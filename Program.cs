055362f540c76d5737c392b5fd0194639c1c1748
Meghrig <76222303+meghrigJAC@users.noreply.github.com>
2023-03-19 10:43:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SideEffects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 6, 4, 3, 1, 7, 2, 10 };
+
+            //Getting the 3 least numbers
+            List<int> smallests = GetSmallests(numbers, 3);
+
+            PrintList("Smallest List:", smallests);
+        }
+
+        static List<int> GetSmallests(List<int> list, int count)
+        {
+            List<int> smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                int min = GetSmallestElement(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+
+            return smallests;
+
+        }
+
+         static int GetSmallestElement(List<int> list)
+        {
+            //Assuming the first element is the smallest
+            var min = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > min)
+                    min = list[i];
+            }
+            return min;
+        }
+        static void PrintList(string listName, List<int> list)
+        {
+            Console.WriteLine(listName);
+            foreach (int number in list)
+                Console.WriteLine(number);
+
+        }
+    }
+}
